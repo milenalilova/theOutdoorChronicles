91e1cc806ffa3e3ead449a2ae8417fbf316f38ca
milenalilova <milenalilova78@gmail.com>
2024-11-12 21:43:27 -0300

    Refactored user creation form

--- theOutdoorChronicles/theOutdoorChronicles/accounts/forms.py
+++ theOutdoorChronicles/theOutdoorChronicles/accounts/forms.py
@@ -6,9 +6,49 @@ UserModel = get_user_model()
 
 
 class AppUserCreationForm(UserCreationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Enter your username',
+            }
+        ),
+        label='Username',
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'placeholder': 'Enter your email',
+            }
+        ),
+        label='Email',
+    )
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Enter your password',
+            }
+        ),
+        label='Password',
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Confirm your password',
+            }
+        ),
+        label='Confirm Password',
+    )
+
     class Meta(UserCreationForm.Meta):
         model = UserModel
-        fields = ('username', 'email', )
+        fields = ('username', 'email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
 
 #      TODO fix how the form displays in the template
 
