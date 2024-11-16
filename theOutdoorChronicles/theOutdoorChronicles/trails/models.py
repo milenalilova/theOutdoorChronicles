a49970c41eb9a66aed47fdee77a6f060e54cba9e
milenalilova <milenalilova78@gmail.com>
2024-11-16 07:57:13 -0300

    Created trail log model

--- theOutdoorChronicles/theOutdoorChronicles/trails/models.py
+++ theOutdoorChronicles/theOutdoorChronicles/trails/models.py
@@ -1,4 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+from theOutdoorChronicles.common.mixins import TimeStampMixin
+
+UserModel = get_user_model()
 
 
 class Trail(models.Model):
@@ -59,3 +64,55 @@ class Trail(models.Model):
         null=False,
         blank=False
     )
+
+
+class TrailLog(TimeStampMixin, models.Model):
+    title = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+
+    date_completed = models.DateField(
+        null=False,
+        blank=False
+    )
+
+    duration = models.DurationField(
+        null=True,
+        blank=True
+    )
+
+    weather_conditions = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+    trail_conditions = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+    difficulty_rating = models.CharField(
+        max_length=100,
+        null=True,
+        blank=True
+    )
+
+    notes = models.TextField(
+        null=False,
+        blank=False
+    )
+
+    private = models.BooleanField(
+        default=False
+    )
+
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+    trail = models.ForeignKey(Trail, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-date_completed']
