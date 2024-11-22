fd561e42af344945c3afcc3f544bf816b968bc24
milenalilova <milenalilova78@gmail.com>
2024-11-22 16:18:20 -0300

    Modified trail log list view. Added get queryset and context

--- theOutdoorChronicles/theOutdoorChronicles/trail_logs/views.py
+++ theOutdoorChronicles/theOutdoorChronicles/trail_logs/views.py
@@ -1,9 +1,11 @@
 from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count, Sum
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, DeleteView
 
+from theOutdoorChronicles.animals.models import Animal
 from theOutdoorChronicles.trail_logs.forms import TrailLogCreateForm, TrailLogEditForm, TrailLogDeleteForm
 from theOutdoorChronicles.trail_logs.models import TrailLog
 from theOutdoorChronicles.trails.models import Trail
@@ -65,8 +67,11 @@ class TrailLogDetailsView(DetailView):
 
     def get_queryset(self):
         return TrailLog.objects.filter(user=self.request.user) \
-            .select_related('trail') \
-            .order_by('-date_completed')
+            .select_related('trail')
+
+    # TODO add next and previous log, or go back to all logs
+
+    # TODO check the queryset
 
 
 class TrailSpecificLogView(ListView):  # every time the user hiked this trail
@@ -79,11 +84,35 @@ class TrailLogListView(ListView):  # all hiking user experience
     paginate_by = 5
     template_name = 'trail_logs/trail-log-list-page.html'
 
-    # def get_queryset(self):
-    #     pass
+    def get_queryset(self):
+        return TrailLog.objects.filter(user=self.request.user) \
+            .select_related('trail') \
+            .prefetch_related('animals', 'photos') \
+            .order_by('-date_completed')
 
-    # def get_context_data(self, **kwargs):
-    #     pass
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        total_trails = TrailLog.objects.filter(user=self.request.user) \
+            .values('trail') \
+            .distinct() \
+            .count()
+
+        # uses reverse relation from Trail
+        total_length = Trail.objects.filter(trail_logs__user=self.request.user) \
+                           .aggregate(total_length=Sum('length'))['total_length'] or 0
+        # reverse relation from Animal
+        total_species_seen = Animal.objects.filter(trail_logs__user=self.request.user) \
+            .distinct() \
+            .count()
+
+        context['total_trails'] = total_trails
+        context['total_length'] = total_length
+        context['total_species_seen'] = total_species_seen
+
+        return context
+
+#  TODO use animals and photos from get_queryset
 
 
 class TrailLogEditView(UpdateView):
